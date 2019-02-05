fa7b2f1d26b133350c9064155088f3275d7be185
viktinho56 <viktinho56@gmail.com>
2019-02-05 08:07:45 +0100

    Update productsViewModel.js

--- viewmodel/productsViewModel.js
+++ viewmodel/productsViewModel.js
@@ -2,7 +2,7 @@
 var bodyParser = require("body-parser");
 var urlencodedParser = bodyParser.urlencoded({extended:false});
 var mongoose =require("mongoose");
-mongoose.connect('mongodb://tinsoft:Hidemyass2@ds030500.mlab.com:30500/tinstore');
+mongoose.connect('process.env.MONGOLAB_URI');
 var productsSchema = new mongoose.Schema({
 
     name:String,
